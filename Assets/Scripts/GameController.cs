d827bd5df966a064c1fa5b66fe6b0acf0d963034
Michael Prokofyev <mikeprokofyev@gmail.com>
2018-05-22 07:11:35 +0300

    enemies spawning

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,20 +7,24 @@ public class GameController : SingletonComponent<GameController> {
 
     [SerializeField] float aiActCooldown = 1f;
     [SerializeField] float enemySpawnCooldown = 1f;
-    [SerializeField] Transform spawnPoints;
-    [SerializeField] Enemy enemyPrefabs;
+    [SerializeField] Enemy[] enemyPrefabs;
+    [SerializeField] Transform enemiesHolder;
+
+    [SerializeField] Transform bottomLeftSpawn;
+    [SerializeField] Transform topLeftSpawn;
+    [SerializeField] Transform topRightSpawn;
+    [SerializeField] Transform bottomRightSpawn;
 
     private float lastAITurnTime = 0;
+    private float lastEnemySpawn = 0;
 
     public Action OnAIAction = () => { };
 
 
-    // Use this for initialization
     void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
         if (lastAITurnTime <= Time.time)
         {
@@ -28,11 +32,56 @@ public class GameController : SingletonComponent<GameController> {
             lastAITurnTime = Time.time + aiActCooldown;
         }
 
-        if (enemySpawnCooldown <= Time.time)
+        if (lastEnemySpawn <= Time.time)
         {
-            
+            //var thisTrySpawnPoints = new List<Transform>(spawnPoints);
+            System.Func<Vector2Int, Enemy, bool> trySpawn = (spawnPoint, enemy) =>
+            {
+                //Vector2 offsetSpawnPoint = spawnPoint.position.xy() - new Vector2(enemy.size.x - 1, enemy.size.y - 1);
+                var foundEntities = Inventory.Instance.AtRect<EntityBase>(spawnPoint, enemy.size.x, enemy.size.y, Inventory.ScanDirection.NONE);
+                if (foundEntities.Count == 0)
+                {
+                    var newEnemy = Instantiate(enemy, enemiesHolder) as Enemy;
+                    newEnemy.Position = spawnPoint;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
 
-            lastAITurnTime = Time.time + aiActCooldown;
+            bool spawned = false;
+            int numberOfTries = 0;
+            var randomEnemy = enemyPrefabs[UnityEngine.Random.Range(0, enemyPrefabs.Length - 1)];
+            while (spawned == false)
+            {
+                Vector2Int offsetSpawn = Vector2Int.zero;
+                float rand = UnityEngine.Random.value;
+                if (rand < .25f)
+                {
+                    offsetSpawn = bottomLeftSpawn.position.xy().Int();
+                }
+                else if (rand < .5f)
+                {
+                    offsetSpawn = topLeftSpawn.position.xy().Int() - new Vector2Int(0, randomEnemy.size.y - 1);
+                }
+                else if (rand < .75f)
+                {
+                    offsetSpawn = topRightSpawn.position.xy().Int() - new Vector2Int(randomEnemy.size.x - 1, randomEnemy.size.y - 1);
+                }
+                else if (rand < 1f)
+                {
+                    offsetSpawn = bottomRightSpawn.position.xy().Int() - new Vector2Int(randomEnemy.size.x - 1, 0);
+                }
+                spawned = trySpawn(offsetSpawn, randomEnemy);
+                numberOfTries++;
+                if (numberOfTries > 4)
+                {
+                    break;
+                }
+            }
+            lastEnemySpawn = Time.time + enemySpawnCooldown;
         }
     }
 
---
+++ Assets/Scripts/Helpers/VectorExtensions.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+
+public static class VectorExtensions
+{
+
+    public static Vector2 xy(this Vector3 v3)
+    {
+        return new Vector2(v3.x, v3.y);
+    }
+    public static Vector2 xz(this Vector3 v3)
+    {
+        return new Vector2(v3.x, v3.z);
+    }
+    public static Vector2 yx(this Vector3 v3)
+    {
+        return new Vector2(v3.y, v3.x);
+    }
+    public static Vector2 yz(this Vector3 v3)
+    {
+        return new Vector2(v3.y, v3.z);
+    }
+    public static Vector2 zx(this Vector3 v3)
+    {
+        return new Vector2(v3.z, v3.x);
+    }
+    public static Vector2 zy(this Vector3 v3)
+    {
+        return new Vector2(v3.z, v3.y);
+    }
+
+    public static Vector3 xy_(this Vector2 v2, float a = 0)
+    {
+        return new Vector3(v2.x, v2.y, a);
+    }
+    public static Vector3 x_y(this Vector2 v2, float a = 0)
+    {
+        return new Vector3(v2.x, a, v2.y);
+    }
+    public static Vector3 _xy(this Vector2 v2, float a = 0)
+    {
+        return new Vector3(a, v2.x, v2.y);
+    }
+    public static Vector3 yx_(this Vector2 v2, float a = 0)
+    {
+        return new Vector3(v2.y, v2.x, a);
+    }
+    public static Vector3 y_x(this Vector2 v2, float a = 0)
+    {
+        return new Vector3(v2.y, a, v2.x);
+    }
+    public static Vector3 _yx(this Vector2 v2, float a = 0)
+    {
+        return new Vector3(a, v2.y, v2.x);
+    }
+
+    public static Vector2 Rotated(this Vector2 v2, float angle)
+    {
+        float cs = Mathf.Cos(angle);
+        float sn = Mathf.Sin(angle);
+        float x = v2.x * cs - v2.y * sn; // now x is something different than original vector x
+        float y = v2.x * sn + v2.y * cs;
+        v2.x = x;
+        v2.y = y;
+        return v2;
+    }
+
+    public static Vector2Int Int(this Vector2 v2)
+    {
+        return new Vector2Int((int)v2.x, (int)v2.y);
+    }
+
+}
